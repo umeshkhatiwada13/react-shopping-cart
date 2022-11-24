fb2cce4370088c90bfdc608a2d8dba2fccaf6e2a
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2022-11-24 15:19:11 +0545

    feat: addition of cart component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Cart from './components/Cart';
 import Header from './components/Header';
 import "./styles/app.scss";
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,54 @@
 import React from 'react';
+const img1 =
+  "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
+const img2 =
+  "https://cdn.shopify.com/s/files/1/2428/5565/products/Neemans-HaleBlack-ReLive-Knits-Jogger-FrontRightLogo-Comfortable-Shoes_1024x.jpg?v=1662876260";
+
 
 const Home = () => {
-    const productList = [{
-        id: 1,
-        name: "Mac",
-        price: 200,
-        imgSrc: "test",
-        handler: "test123"
-    },
-    {
-        id: 2,
-        name: "Earphone",
-        price: 50,
-        imgSrc: "test"
-    },]
-    return (
-        <div className='home'>
-            {
-                productList.map((i) => (
-                    <ProductCard
-                        id={i.id}
-                        key={i.id}
-                        name={i.name}
-                        price={i.price}
-                        imgSrc={i.imgSrc}
-                        handler={i.handler}
-                    />
-                ))
-            }
-        </div>
-    );
+  const productList = [{
+    id: 1,
+    name: "Mac",
+    price: 200,
+    imgSrc: img1
+  },
+  {
+    id: 2,
+    name: "Earphone",
+    price: 50,
+    imgSrc: img2
+  },]
+
+  const addToCartHandler = (options) => {
+    console.log(options);
+  }
+
+  return (
+    <div className='home'>
+      {
+        productList.map((i) => (
+          <ProductCard
+            id={i.id}
+            key={i.id}
+            name={i.name}
+            price={i.price}
+            imgSrc={i.imgSrc}
+            handler={addToCartHandler}
+          />
+        ))
+      }
+    </div>
+  );
 };
 
-const ProductCard = ({ id, name, price, imgSrc, handler }) => {
-    console.log('data ', name);
-    <div className="productCard">
-        <img src={imgSrc} alt={name} />
-        <p>{name}</p>
-        <h4>${price}</h4>
-        <button onClick={() => handler({ id, name, price, quantity: 1, imgSrc })}>Add to Cart</button>
-    </div>
-}
+const ProductCard = ({ id, name, price, imgSrc, handler }) => (
+  // console.log('data ', name);
+  <div className="productCard">
+    <img src={imgSrc} alt={name} />
+    <p>{name}</p>
+    <h4>${price}</h4>
+    <button onClick={() => handler({ id, name, price, quantity: 1, imgSrc })}>Add to Cart</button>
+  </div>
+)
 
 export default Home;

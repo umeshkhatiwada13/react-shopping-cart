741eb2c8d7fbedf443aaa0fe88215bfbe8a43d13
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2022-11-24 14:04:13 +0545

    working on card component on home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Header from './components/Header';
 import "./styles/app.scss";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
-  ); 
+  );
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
 const Home = () => {
+    const productList = [{
+        id: 1,
+        name: "Mac",
+        price: 200,
+        imgSrc: "test",
+        handler: "test123"
+    },
+    {
+        id: 2,
+        name: "Earphone",
+        price: 50,
+        imgSrc: "test"
+    },]
     return (
-        <div>Home</div>
-    )
+        <div className='home'>
+            {
+                productList.map((i) => (
+                    <ProductCard
+                        id={i.id}
+                        key={i.id}
+                        name={i.name}
+                        price={i.price}
+                        imgSrc={i.imgSrc}
+                        handler={i.handler}
+                    />
+                ))
+            }
+        </div>
+    );
+};
+
+const ProductCard = ({ id, name, price, imgSrc, handler }) => {
+    console.log('data ', name);
+    <div className="productCard">
+        <img src={imgSrc} alt={name} />
+        <p>{name}</p>
+        <h4>${price}</h4>
+        <button onClick={() => handler({ id, name, price, quantity: 1, imgSrc })}>Add to Cart</button>
+    </div>
 }
 
-export default Home
+export default Home;
